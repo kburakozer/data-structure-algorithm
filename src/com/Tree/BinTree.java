5ad6f1b5f5c4d22c038a47d978995b86f7c0b361
kburakozer <kburakozer@yahoo.com>
2020-10-05 17:42:40 +0300

    tree traversal methods added

--- src/com/Tree/BinTree.java
+++ src/com/Tree/BinTree.java
@@ -1,5 +1,11 @@
 package com.Tree;
 
+import com.queue.MyQueue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class BinTree {
 
 
@@ -38,5 +44,46 @@ public class BinTree {
 
     public BinNode getRoot() {
         return root;
+    }
+
+    public void breadthFirstSearch(){
+        if(root == null){
+            System.out.println("The tree is empty");
+            return;
+        }
+        Queue<BinNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()){
+            BinNode current = queue.remove();
+            if(current.getLeft() != null){
+                queue.add(current.getLeft());
+            }
+            if(current.getRight() != null){
+                queue.add(current.getRight());
+            }
+            System.out.println(current.getValue());
+        }
+
+    }
+
+    public void depthFirstSearch(){
+        if(root == null){
+            System.out.println("The tree is empty");
+            return;
+        }
+        Stack<BinNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            BinNode current = stack.pop();
+            if(current.getRight() != null){
+                stack.push(current.getRight());
+            }
+
+            if(current.getLeft() != null){
+                stack.push(current.getLeft());
+            }
+            System.out.println(current.getValue());
+        }
+
     }
 }
--- src/com/Tree/Main.java
+++ src/com/Tree/Main.java
@@ -1,15 +1,19 @@
 package com.Tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
         BinTree tree = new BinTree();
+        tree.add(15);
+        tree.add(10);
         tree.add(8);
-        tree.add(7);
-        tree.add(9);
-
-        System.out.println(tree.getRoot().getValue());
-        System.out.println(tree.getRoot().getRight().getValue());
-        System.out.println(tree.getRoot().getLeft().getValue());
-
+        tree.add(12);
+        tree.add(17);
+        tree.add(16);
+        tree.add(18);
+        tree.depthFirstSearch();
     }
 }
